860705f07b9a8aecd00908694472e8397f4594b4
Brett208 <15183337+Brett208@users.noreply.github.com>
2019-05-26 21:28:04 -0400

    Remove deprecated calls to OP2Helper function CreateWallOrTubeLine

--- HumanBase.cpp
+++ HumanBase.cpp
@@ -109,10 +109,10 @@ void InitializeStartingUnits(UnitHelper::VehicleBuilder &vehicleBuilder)
 	TethysGame::CreateUnit(building, map_id::mapCommonOreSmelter, LOCATION(76 + X_, 11 + Y_), Player0, map_id::mapNone, 0);
 	vehicleBuilder.SetPercentDamaged(building, 33);
 
-	CreateTubeOrWallLine(76 + X_, 14 + Y_, 76 + X_, 19 + Y_, map_id::mapTube);
-	CreateTubeOrWallLine(78 + X_, 19 + Y_, 78 + X_, 19 + Y_, map_id::mapTube);
-	CreateTubeOrWallLine(38 + X_, 71 + Y_, 39 + X_, 71 + Y_, map_id::mapTube);
-	CreateTubeOrWallLine(40 + X_, 68 + Y_, 40 + X_, 69 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(76 + X_, 14 + Y_), LOCATION(76 + X_, 19 + Y_));
+	CreateTubeLine(LOCATION(78 + X_, 19 + Y_), LOCATION(78 + X_, 19 + Y_));
+	CreateTubeLine(LOCATION(38 + X_, 71 + Y_), LOCATION(39 + X_, 71 + Y_));
+	CreateTubeLine(LOCATION(40 + X_, 68 + Y_), LOCATION(40 + X_, 69 + Y_));
 
 	TethysGame::CreateUnit(building, map_id::mapStructureFactory, LOCATION(40 + X_, 65 + Y_), Player0, map_id::mapNone, 0);
 	building.SetFactoryCargo(0, map_id::mapStructureFactory, map_id::mapNone);
@@ -120,12 +120,12 @@ void InitializeStartingUnits(UnitHelper::VehicleBuilder &vehicleBuilder)
 	building.SetFactoryCargo(2, map_id::mapRareStorage, map_id::mapNone);
 	vehicleBuilder.SetPercentDamaged(building, 72);
 
-	CreateTubeOrWallLine(43 + X_, 65 + Y_, 47 + X_, 65 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(43 + X_, 65 + Y_), LOCATION(47 + X_, 65 + Y_));
 
 	TethysGame::CreateUnit(building, map_id::mapVehicleFactory, LOCATION(29 + X_, 79 + Y_), Player0, map_id::mapNone, 0);
 	vehicleBuilder.SetPercentDamaged(building, 30);
 
-	CreateTubeOrWallLine(32 + X_, 79 + Y_, 35 + X_, 79 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(32 + X_, 79 + Y_), LOCATION(35 + X_, 79 + Y_));
 
 	TethysGame::CreateUnit(building, map_id::mapGORF, LOCATION(44 + X_, 55 + Y_), Player0, map_id::mapNone, 0);
 	vehicleBuilder.SetPercentDamaged(building, 54);
@@ -134,10 +134,10 @@ void InitializeStartingUnits(UnitHelper::VehicleBuilder &vehicleBuilder)
 	vehicleBuilder.SetPercentDamaged(building, 93);
 
 	//GORF to RCC
-	CreateTubeOrWallLine(44 + X_, 57 + Y_, 44 + X_, 58 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(44 + X_, 57 + Y_), LOCATION(44 + X_, 58 + Y_));
 
 	//RCC to Struct Factory
-	CreateTubeOrWallLine(45 + X_, 62 + Y_, 45 + X_, 64 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(45 + X_, 62 + Y_), LOCATION(45 + X_, 64 + Y_));
 
 	TethysGame::CreateUnit(building, map_id::mapResidence, LOCATION(23 + X_, 74 + Y_), Player0, map_id::mapNone, 0);
 	vehicleBuilder.SetPercentDamaged(building, 80);
@@ -187,10 +187,10 @@ void InitializeStartingUnits(UnitHelper::VehicleBuilder &vehicleBuilder)
 	TethysGame::CreateUnit(building, map_id::mapCommonStorage, LOCATION(38 + X_, 55 + Y_), Player0, map_id::mapNone, 0);
 	vehicleBuilder.SetPercentDamaged(building, 35);
 
-	CreateTubeOrWallLine(40 + X_, 55 + Y_, 42 + X_, 55 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(40 + X_, 55 + Y_), LOCATION(42 + X_, 55 + Y_));
 
 	rubbleMaker.CreateDestroyedBuilding(map_id::mapResidence, LOCATION(27 + X_, 74 + Y_));
-	CreateTubeOrWallLine(27 + X_, 76 + Y_, 27 + X_, 77 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(27 + X_, 76 + Y_), LOCATION(27 + X_, 77 + Y_));
 
 	rubbleMaker.CreateDestroyedBuilding(map_id::mapCommandCenter, LOCATION(49 + X_, 66 + Y_));
 	rubbleMaker.CreateDestroyedBuilding(map_id::mapVehicleFactory, LOCATION(38 + X_, 79 + Y_));
@@ -202,18 +202,18 @@ void InitializeStartingUnits(UnitHelper::VehicleBuilder &vehicleBuilder)
 	rubbleMaker.CreateDestroyedBuilding(map_id::mapRecreationFacility, LOCATION(45 + X_, 44 + Y_));
 
 	// Destroyed Rec Center to Destroyed University
-	CreateTubeOrWallLine(45 + X_, 47 + Y_, 45 + X_, 45 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(45 + X_, 47 + Y_), LOCATION(45 + X_, 45 + Y_));
 
 	// Rec Center to Destroyed Rec Center
-	CreateTubeOrWallLine(42 + X_, 44 + Y_, 43 + X_, 44 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(42 + X_, 44 + Y_), LOCATION(43 + X_, 44 + Y_));
 
 	rubbleMaker.CreateDestroyedBuilding(map_id::mapGarage, LOCATION(39 + X_, 87 + Y_));
 	
 	rubbleMaker.CreateDestroyedBuilding(map_id::mapNursery, LOCATION(40 + X_, 49 + Y_));
 	rubbleMaker.CreateDestroyedBuilding(map_id::mapUniversity, LOCATION(45 + X_, 49 + Y_));
 
-	CreateTubeOrWallLine(44 + X_, 51 + Y_, 44 + X_, 53 + Y_, map_id::mapTube);
-	CreateTubeOrWallLine(42 + X_, 49 + Y_, 43 + X_, 49 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(44 + X_, 51 + Y_), LOCATION(44 + X_, 53 + Y_));
+	CreateTubeLine(LOCATION(42 + X_, 49 + Y_), LOCATION(43 + X_, 49 + Y_));
 
 	TethysGame::CreateUnit(building, map_id::mapGuardPost, LOCATION(35 + X_, 42 + Y_), Player0, map_id::mapESG, 0);
 	vehicleBuilder.SetPercentDamaged(building, 6);
@@ -221,7 +221,7 @@ void InitializeStartingUnits(UnitHelper::VehicleBuilder &vehicleBuilder)
 	TethysGame::CreateUnit(building, map_id::mapGuardPost, LOCATION(28 + X_, 42 + Y_), Player0, map_id::mapRPG, 0);
 	vehicleBuilder.SetPercentDamaged(building, 0);
 
-	CreateTubeOrWallLine(30 + X_, 42 + Y_, 34 + X_, 42 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(30 + X_, 42 + Y_), LOCATION(34 + X_, 42 + Y_));
 
 	rubbleMaker.CreateDestroyedBuilding(map_id::mapCommonOreMine, LOCATION(6 + X_, 112 + Y_));
 	rubbleMaker.CreateDestroyedBuilding(map_id::mapCommonOreMine, LOCATION(91 + X_, 4 + Y_));
@@ -241,86 +241,86 @@ void InitializeStartingUnits(UnitHelper::VehicleBuilder &vehicleBuilder)
 	vehicleBuilder.SetPercentDamaged(building, 35);
 
 	//From Agridome to destroyed Vech factory
-	CreateTubeOrWallLine(35 + X_, 73 + Y_, 35 + X_, 75 + Y_, map_id::mapTube);
-	CreateTubeOrWallLine(36 + X_, 75 + Y_, 37 + X_, 75 + Y_, map_id::mapTube);
-	CreateTubeOrWallLine(37 + X_, 76 + Y_, 37 + X_, 77 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(35 + X_, 73 + Y_), LOCATION(35 + X_, 75 + Y_));
+	CreateTubeLine(LOCATION(36 + X_, 75 + Y_), LOCATION(37 + X_, 75 + Y_));
+	CreateTubeLine(LOCATION(37 + X_, 76 + Y_), LOCATION(37 + X_, 77 + Y_));
 
 	//From destroyed vech factory to destroyed garage.
-	CreateTubeOrWallLine(38 + X_, 82 + Y_, 38 + X_, 85 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(38 + X_, 82 + Y_), LOCATION(38 + X_, 85 + Y_));
 
 	//From destroyed garage to garage
-	CreateTubeOrWallLine(34 + X_, 87 + Y_, 37 + X_, 87 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(34 + X_, 87 + Y_), LOCATION(37 + X_, 87 + Y_));
 
 	//Destroyed Nursery to GORF/Common ore storage.
-	CreateTubeOrWallLine(40 + X_, 51 + Y_, 40 + X_, 54 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(40 + X_, 51 + Y_), LOCATION(40 + X_, 54 + Y_));
 
 	//University to Garage to Destroyed Agridome
-	CreateTubeOrWallLine(47 + X_, 49 + Y_, 80 + X_, 49 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(47 + X_, 49 + Y_), LOCATION(80 + X_, 49 + Y_));
 
 	//Residence to Agridome
-	CreateTubeOrWallLine(61 + X_, 40 + Y_, 61 + X_, 42 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(61 + X_, 40 + Y_), LOCATION(61 + X_, 42 + Y_));
 
 	//4 Residences (3 destroyed)
-	CreateTubeOrWallLine(62 + X_, 40 + Y_, 81 + X_, 40 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(62 + X_, 40 + Y_), LOCATION(81 + X_, 40 + Y_));
 
 	//4 Residences to Destroyed Spaceport
-	CreateTubeOrWallLine(81 + X_, 31 + Y_, 81 + X_, 49 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(81 + X_, 31 + Y_), LOCATION(81 + X_, 49 + Y_));
 
 	//Command Center to Agridome Alternate Route
-	CreateTubeOrWallLine(49 + X_, 68 + Y_, 49 + X_, 71 + Y_, map_id::mapTube);
-	CreateTubeOrWallLine(48 + X_, 71 + Y_, 44 + X_, 71 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(49 + X_, 68 + Y_), LOCATION(49 + X_, 71 + Y_));
+	CreateTubeLine(LOCATION(48 + X_, 71 + Y_), LOCATION(44 + X_, 71 + Y_));
 
 	//Completes GORF to Destroyed University
 	TethysGame::CreateWallOrTube(45 + X_, 51 + Y_, -1, map_id::mapTube);
 
 	//Rare Ore Smelter to Rare Ore Smelter
-	CreateTubeOrWallLine(21 + X_, 103 + Y_, 21 + X_, 105 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(21 + X_, 103 + Y_), LOCATION(21 + X_, 105 + Y_));
 
 	rubbleMaker.CreateDestroyedBuilding(map_id::mapStandardLab, LOCATION(39 + X_, 93 + Y_));
 	rubbleMaker.CreateDestroyedBuilding(map_id::mapAdvancedLab, LOCATION(66 + X_, 29 + Y_));
 
 	//Destroyed Advanced Lab to Destroyed Residence
-	CreateTubeOrWallLine(66 + X_, 36 + Y_, 66 + X_, 32 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(66 + X_, 36 + Y_), LOCATION(66 + X_, 32 + Y_));
 
 	//Destroyed Garage to Destroyed Standard Lab
-	CreateTubeOrWallLine(39 + X_, 89 + Y_, 39 + X_, 91 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(39 + X_, 89 + Y_), LOCATION(39 + X_, 91 + Y_));
 
 	TethysGame::CreateUnit(building, map_id::mapBasicLab, LOCATION(39 + X_, 99 + Y_), Player0, map_id::mapNone, 0);
 	vehicleBuilder.SetPercentDamaged(building, 16);
 
 	//Destroyed Standard Lab to Basic Lab
-	CreateTubeOrWallLine(39 + X_, 95 + Y_, 39 + X_, 98 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(39 + X_, 95 + Y_), LOCATION(39 + X_, 98 + Y_));
 
 	//Basic Lab to Destroyed Arachnid Factory to Destroyed RCC
-	CreateTubeOrWallLine(41 + X_, 99 + Y_, 53 + X_, 99 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(41 + X_, 99 + Y_), LOCATION(53 + X_, 99 + Y_));
 
 	//Destroyed RCC to Arachnid Factory
-	CreateTubeOrWallLine(53 + X_, 100 + Y_, 53 + X_, 102 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(53 + X_, 100 + Y_), LOCATION(53 + X_, 102 + Y_));
 
 	rubbleMaker.CreateDestroyedBuilding(map_id::mapGORF, LOCATION(31 + X_, 98 + Y_));
 
 	//Basic Lab to Destroyed GORF
-	CreateTubeOrWallLine(34 + X_, 98 + Y_, 37 + X_, 98 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(34 + X_, 98 + Y_), LOCATION(37 + X_, 98 + Y_));
 
 	//Destroyed Rare Ore Smelter to Destroyed GORF
-	CreateTubeOrWallLine(24 + X_, 100 + Y_, 31 + X_, 100 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(24 + X_, 100 + Y_), LOCATION(31 + X_, 100 + Y_));
 
 	rubbleMaker.CreateDestroyedBuilding(map_id::mapSpaceport, LOCATION(77 + X_, 29 + Y_));
 
 	rubbleMaker.CreateDestroyedBuilding(map_id::mapRareOreSmelter, LOCATION(32 + X_, 107 + Y_));
 
 	//Rare Ore Smelter to Destroyed Rare Ore Smelter
-	CreateTubeOrWallLine(23 + X_, 110 + Y_, 32 + X_, 110 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(23 + X_, 110 + Y_), LOCATION(32 + X_, 110 + Y_));
 
 	rubbleMaker.CreateDestroyedBuilding(map_id::mapMHDGenerator, LOCATION(53 + X_, 76 + Y_));
 
 	rubbleMaker.CreateDestroyedBuilding(map_id::mapTradeCenter, LOCATION(47 + X_, 87 + Y_));
 
 	//From Destroyed Garage to Destroyed Trade Center
-	CreateTubeOrWallLine(42 + X_, 87 + Y_, 45 + X_, 87 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(42 + X_, 87 + Y_), LOCATION(45 + X_, 87 + Y_));
 
 	//From Destroyed Trade Center to Destroyed Arachnid Factory
-	CreateTubeOrWallLine(47 + X_, 89 + Y_, 47 + X_, 95 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(47 + X_, 89 + Y_), LOCATION(47 + X_, 95 + Y_));
 
 	rubbleMaker.CreateDestroyedBuilding(map_id::mapArachnidFactory, LOCATION(47 + X_, 97 + Y_));
 	rubbleMaker.CreateDestroyedBuilding(map_id::mapSolarPowerArray, LOCATION(50 + X_, 39 + Y_));
@@ -331,22 +331,22 @@ void InitializeStartingUnits(UnitHelper::VehicleBuilder &vehicleBuilder)
 	rubbleMaker.CreateDestroyedBuilding(map_id::mapRobotCommand, LOCATION(53 + X_, 97 + Y_));
 
 	//From Spaceport to Destroyed Common Ore Smelter
-	CreateTubeOrWallLine(81 + X_, 22 + Y_, 81 + X_, 30 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(81 + X_, 22 + Y_), LOCATION(81 + X_, 30 + Y_));
 
 	rubbleMaker.CreateDestroyedBuilding(map_id::mapGuardPost, LOCATION(95 + X_, 33 + Y_));
 
-	CreateTubeOrWallLine(95 + X_, 35 + Y_, 95 + X_, 38 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(95 + X_, 35 + Y_), LOCATION(95 + X_, 38 + Y_));
 
 	rubbleMaker.CreateDestroyedBuilding(map_id::mapGuardPost, LOCATION(95 + X_, 39 + Y_));
 
-	CreateTubeOrWallLine(95 + X_, 41 + Y_, 82 + X_, 41 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(95 + X_, 41 + Y_), LOCATION(82 + X_, 41 + Y_));
 
 	rubbleMaker.CreateDestroyedBuilding(map_id::mapDIRT, LOCATION(69 + X_, 47 + Y_));
 	rubbleMaker.CreateDestroyedBuilding(map_id::mapDIRT, LOCATION(75 + X_, 47 + Y_));
 
 	rubbleMaker.CreateDestroyedBuilding(map_id::mapGuardPost, LOCATION(96 + X_, 67 + Y_));
 
-	CreateTubeOrWallLine(96 + X_, 69 + Y_, 96 + X_, 73 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(96 + X_, 69 + Y_), LOCATION(96 + X_, 73 + Y_));
 
 	rubbleMaker.CreateDestroyedBuilding(map_id::mapGuardPost, LOCATION(96 + X_, 74 + Y_));
 	rubbleMaker.CreateDestroyedBuilding(map_id::mapGuardPost, LOCATION(91 + X_, 95 + Y_));
@@ -355,7 +355,7 @@ void InitializeStartingUnits(UnitHelper::VehicleBuilder &vehicleBuilder)
 
 	TethysGame::CreateWallOrTube(77 + X_, 19 + Y_, -1, map_id::mapTube);
 
-	CreateTubeOrWallLine(55 + X_, 97 + Y_, 91 + X_, 97 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(55 + X_, 97 + Y_), LOCATION(91 + X_, 97 + Y_));
 
 	rubbleMaker.CreateDestroyedBuilding(map_id::mapMHDGenerator, LOCATION(46 + X_, 120 + Y_));
 
@@ -364,33 +364,33 @@ void InitializeStartingUnits(UnitHelper::VehicleBuilder &vehicleBuilder)
 	TethysGame::CreateUnit(building, map_id::mapDIRT, LOCATION(60 + X_, 89 + Y_), Player0, map_id::mapNone, 0);
 	vehicleBuilder.SetPercentDamaged(building, 30);
 
-	CreateTubeOrWallLine(60 + X_, 91 + Y_, 60 + X_, 93 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(60 + X_, 91 + Y_), LOCATION(60 + X_, 93 + Y_));
 
 	TethysGame::CreateUnit(building, map_id::mapResidence, LOCATION(67 + X_, 56 + Y_), Player0, map_id::mapNone, -1);
 	vehicleBuilder.SetPercentDamaged(building, 30);
 
-	CreateTubeOrWallLine(52 + X_, 66 + Y_, 68 + X_, 66 + Y_, map_id::mapTube);
-	CreateTubeOrWallLine(67 + X_, 58 + Y_, 67 + X_, 60 + Y_, map_id::mapTube);
-	CreateTubeOrWallLine(96 + X_, 76 + Y_, 91 + X_, 76 + Y_, map_id::mapTube);
-	CreateTubeOrWallLine(91 + X_, 76 + Y_, 91 + X_, 94 + Y_, map_id::mapTube);
-	CreateTubeOrWallLine(37 + X_, 42 + Y_, 39 + X_, 42 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(52 + X_, 66 + Y_), LOCATION(68 + X_, 66 + Y_));
+	CreateTubeLine(LOCATION(67 + X_, 58 + Y_), LOCATION(67 + X_, 60 + Y_));
+	CreateTubeLine(LOCATION(96 + X_, 76 + Y_), LOCATION(91 + X_, 76 + Y_));
+	CreateTubeLine(LOCATION(91 + X_, 76 + Y_), LOCATION(91 + X_, 94 + Y_));
+	CreateTubeLine(LOCATION(37 + X_, 42 + Y_), LOCATION(39 + X_, 42 + Y_));
 
 	TethysGame::CreateUnit(building, map_id::mapGuardPost, LOCATION(31 + X_, 58 + Y_), Player0, map_id::mapStickyfoam, -1);
 
-	CreateTubeOrWallLine(33 + X_, 58 + Y_, 38 + X_, 58 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(33 + X_, 58 + Y_), LOCATION(38 + X_, 58 + Y_));
 	TethysGame::CreateWallOrTube(38 + X_, 57 + Y_, -1,  map_id::mapTube);
 
 	TethysGame::CreateUnit(building, map_id::mapRareStorage, LOCATION(44 + X_, 39 + Y_), Player0, map_id::mapNone, -1);
 	vehicleBuilder.SetPercentDamaged(building, 70);
 
-	CreateTubeOrWallLine(44 + X_, 41 + Y_, 44 + X_, 42 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(44 + X_, 41 + Y_), LOCATION(44 + X_, 42 + Y_));
 
 	TethysGame::CreateUnit(building, map_id::mapRareStorage, LOCATION(43 + X_, 77 + Y_), Player0, map_id::mapNone, -1);
 	TethysGame::CreateUnit(building, map_id::mapCommonStorage, LOCATION(60 + X_, 102 + Y_), Player0, map_id::mapNone, -1);
 	vehicleBuilder.SetPercentDamaged(building, 70);
 
-	CreateTubeOrWallLine(60 + X_, 98 + Y_, 60 + X_, 100 + Y_, map_id::mapTube);
-	CreateTubeOrWallLine(41 + X_, 79 + Y_, 43 + X_, 79 + Y_, map_id::mapTube);
+	CreateTubeLine(LOCATION(60 + X_, 98 + Y_), LOCATION(60 + X_, 100 + Y_));
+	CreateTubeLine(LOCATION(41 + X_, 79 + Y_), LOCATION(43 + X_, 79 + Y_));
 
 	InitializeBlastedTiles(rubbleMaker);
 
